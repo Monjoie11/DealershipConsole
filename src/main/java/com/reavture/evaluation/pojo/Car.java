9e678819295bcca7b65df0d817f708df993f01be
Monjoie11 <jeff.franken@gmail.com>
2019-09-26 21:24:02 -0400

    inital commit project zero

---
+++ src/main/java/com/reavture/evaluation/pojo/Car.java
@@ -0,0 +1,49 @@
+package com.reavture.evaluation.pojo;
+
+public class Car {
+	
+	private String make;
+	
+	private String model;
+	
+	private int year;
+	
+	private float price;
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+
+}
